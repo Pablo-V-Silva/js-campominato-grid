cd1b8341db8122af4e9f42fbd5f4d0b12a9834f6
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-10-20 18:07:17 +0200

    Addeventlistener completati

--- main.js
+++ main.js
@@ -5,26 +5,92 @@ con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49 */
 
 
-/* Dichiarazione delle statistiche per la difficoltà facile */
+/* Dichiarazione del bottone per la difficoltà facile */
 const easyBtn = document.getElementById('easy')
 
-/* Dichiarazione delle statistiche per la difficoltà media */
+/* Dichiarazione del bottone per la difficoltà media */
 const mediumBtn = document.getElementById('medium')
-const mediumContain = document.getElementById('containerMedium')
 
-/* Dichiarazione delle statistiche per la difficoltà difficile */
+/* Dichiarazione del bottone per la difficoltà difficile */
 const hardBtn = document.getElementById('hard')
-const hardContain = document.getElementById('containerHard')
 
 
+
+/* Dichiarazione degli eventi che genereranno i quadratini in base al bottone che verrà premuto */
 easyBtn.addEventListener('click', function () {
-  const easyContain = document.getElementById('containerEasy')
+  const nSquares = 100;
 
-  for (let i = 0; i < 100; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    easyContain.append(square);
-    console.log(square);
-  }
+  contain.className = 'easyWidth'
+
+  generate(nSquares);
 
 })
+
+mediumBtn.addEventListener('click', function () {
+  const nSquares = 81;
+
+  contain.className = 'mediumWidth'
+
+  generate(nSquares);
+
+})
+
+hardBtn.addEventListener('click', function () {
+  const nSquares = 49;
+
+  contain.className = 'hardWidth'
+
+  generate(nSquares);
+
+})
+
+/* Funzione creata per generare N quadratini in base alla difficoltà che l'utente sceglie di giocare */
+function generate(quadratini) {
+
+  contain.innerHTML = "";
+
+  for (let i = 1; i <= quadratini; i++) {
+    /* let square = `
+    <div class="square purple">
+    ${i}
+    </div>`;
+    contain.innerHTML += square;
+    console.log(square);
+
+    let changeColor = document.getElementsByClassName('square');
+
+    changeColor.addEventListener('click', function () {
+      console.log('cliccato');
+    }) */
+
+    /* Dichiarazione del contenitore dove verranno stampati i quadratini per il gioco */
+    const contain = document.getElementById('contain');
+
+
+    /* creazione del contenitore del quadrato */
+    let square = document.createElement('div');
+    /* Aggiunta delle classi per definire lo stile dei quadrati */
+    square.classList.add('purple', 'square')
+    /* Attacco del quadrato al contenitore padre */
+    contain.append(square);
+    /* Immissione del numero corrispondente al quadrato */
+    square.innerHTML += i
+
+    /* Aggiunta dell'evento al click dei quadrati per :
+    - il primo if : aggiungere la classe "selected" e rimuovere la classe originale del background
+    - il secondo if : rimuovere la classe "selected" e aggiungere di nuovo la classe originale per il background */
+
+    square.addEventListener('click', function () {
+
+      if (square.classList.contains('purple')) {
+        square.classList.remove('purple');
+        square.classList.add('selected');
+      } else if (square.classList.contains('selected')) {
+        square.classList.remove('selected');
+        square.classList.add('purple');
+      }
+    })
+  }
+
+
+}
